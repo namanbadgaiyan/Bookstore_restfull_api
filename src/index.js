c5c4126bb59e0eb1b604e6e77512548dac25e224
naman badgaiyan <namanbadgaiyan03@gmail.com>
2024-09-25 14:06:02 +0530

    database connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+const dotevn = require('dotenv')
+const path = require('path')
+const app = require("./app")
+dotevn.config({path: path.join(__dirname, "..", ".env")});
+require('./databases/database').Connectdatabase()
 
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`server is running on this port ${process.env.PORT}`);
+})
